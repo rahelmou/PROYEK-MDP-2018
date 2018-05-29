66e54a29b0d67b76ad8041fae5d86ae969e4bc51
Rahel Moudy <moudy.sj@gmail.com>
2018-05-29 18:11:41 +0700

    coba seh commit lagi. sign up sudah, login fb blom slsai, google msh gagal

--- tourbook/app/src/main/java/stts/edu/tourbook/HomeActivity.java
+++ tourbook/app/src/main/java/stts/edu/tourbook/HomeActivity.java
@@ -1,21 +1,13 @@
 package stts.edu.tourbook;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
-    Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        btn = findViewById(R.id.button);
-    }
-    public void btnCoba(View v){
-        Intent i = new Intent(this, CreateTourActivity.class);
-        startActivity(i);
     }
 }
